2d160e046d3e7f2a2911bd7d25e746bc388bdef1
upmasingh70@gmail.com <upmasingh70@gmail.com>
2021-01-11 01:06:00 +0530

    custom error messages

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -13,9 +13,12 @@ public class Customer {
 	@Size(min=1,message="is required")
 	private String lastName;
 	
+	
+	
 	@Min(value=0,message="must be greater than or equal to 0")
 	@Max(value=10,message="must be lesser than or equal to 10")
-	private int freePasses;
+	@NotNull(message="is required")
+	private Integer freePasses; 
 	
 	
 	public String getFirstName() {
@@ -30,10 +33,10 @@ public class Customer {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 	
